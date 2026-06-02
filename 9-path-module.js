e0e0495fe548a81d1c45cea9e00711dcd3fed060
sc-forlife <salemsc7@gmail.com>
2026-06-02 07:44:12 +0200

    Testing out gitignore node modules

--- 9-path-module.js
+++ 9-path-module.js
@@ -1,11 +1,15 @@
 const path = require("path");
+//It tells you what character your operating system uses to separate folders.
 console.log(path.sep);
 
+//combines path segments and cleans up extra slashes.
 const filePath = path.join("/content//", "///subfolder/////", "///test.txt///");
 console.log(filePath);
 
+//returns the last part of the path.
 const base = path.basename(filePath);
 console.log(base);
 
+//creates an absolute path.
 const absolute = path.resolve(__dirname, "content", "subfolder", "text.txt");
 console.log(absolute);
