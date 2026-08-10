d0e866edc11cad91f47923012077077ca2e6b147
sc-forlife <salemsc7@gmail.com>
2026-08-10 12:19:23 +0200

    Setting up connections and middleware

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,3 +1,34 @@
 import dotenv from "dotenv";
+import express from "express";
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorMiddleware from "./middleware/error-handler.js";
 
 dotenv.config();
+//async errors
+
+const app = express();
+
+//middleware
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.send("<h1>Store Api</h1><a href='/api/v1/products'>products route</a>");
+});
+
+//products route
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    //connectDB
+    app.listen(port, console.log(`server is listening to the port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
