9d28f947b720a15fb3b9eeb56ad119af7e7e7278
sc-forlife <salemsc7@gmail.com>
2026-08-11 08:01:03 +0200

    Creating routes in controllers

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorMiddleware from "./middleware/error-handler.js";
+import connectDB from "./db/connect.js";
 
 dotenv.config();
 //async errors
@@ -16,6 +17,8 @@ app.get("/", (req, res) => {
   res.send("<h1>Store Api</h1><a href='/api/v1/products'>products route</a>");
 });
 
+app.use("/api/v1/products");
+
 //products route
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
@@ -25,6 +28,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     //connectDB
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`server is listening to the port ${port}...`));
   } catch (error) {
     console.log(error);
